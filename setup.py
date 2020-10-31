5c921744e5b89d7dde9b889f2e05b281b793420c
Hanoona Rasheed <hanoona@uniqueroboticsedu.com>
2020-10-31 06:20:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,18 +19,18 @@ setuptools.setup(
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Education",
-        "License :: OSI Approved :: Apache License, Version 2.0 (Apache-2.0)",
+        "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Operating System :: Microsoft :: Windows :: Windows 10"
-        "Environment :: Win32 (MS Windows)"
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Environment :: Win32 (MS Windows)",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering",
         "Topic :: Utilities",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",       
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     python_requires='>=3.6',
 )
